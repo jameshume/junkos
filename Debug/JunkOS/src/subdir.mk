a945a6950ff320434e42645807389058503164f6
James Hume <james.hume@gmail.com>
2023-10-26 16:23:39 +0100

    Seperate scheduler out into separate file

--- Debug/JunkOS/src/subdir.mk
+++ Debug/JunkOS/src/subdir.mk
@@ -5,31 +5,23 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/main.c \
-../Src/syscalls.c \
-../Src/sysmem.c 
+../JunkOS/src/junkos_scheduler.c 
 
 OBJS += \
-./Src/main.o \
-./Src/syscalls.o \
-./Src/sysmem.o 
+./JunkOS/src/junkos_scheduler.o 
 
 C_DEPS += \
-./Src/main.d \
-./Src/syscalls.d \
-./Src/sysmem.d 
+./JunkOS/src/junkos_scheduler.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/main.o: ../Src/main.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F401RETx -DSTM32F4 -c -I../Inc -I"/home/james/STM32CubeIDE/Play/Play1/JunkOS/inc" -I"/home/james/STM32CubeIDE/Play/Play1/stm32f401ret6_chip_headers/CMSIS/Include" -I"/home/james/STM32CubeIDE/Play/Play1/stm32f401ret6_chip_headers/CMSIS/Device/ST/STM32F4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
-Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
+JunkOS/src/%.o JunkOS/src/%.su: ../JunkOS/src/%.c JunkOS/src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F401RETx -DSTM32F4 -c -I../Inc -I"/home/james/STM32CubeIDE/Play/Play1/stm32f401ret6_chip_headers/CMSIS/Include" -I"/home/james/STM32CubeIDE/Play/Play1/stm32f401ret6_chip_headers/CMSIS/Device/ST/STM32F4xx/Include" -I"/home/james/STM32CubeIDE/Play/Play1/JunkOS/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
-clean: clean-Src
+clean: clean-JunkOS-2f-src
 
-clean-Src:
-	-$(RM) ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+clean-JunkOS-2f-src:
+	-$(RM) ./JunkOS/src/junkos_scheduler.d ./JunkOS/src/junkos_scheduler.o ./JunkOS/src/junkos_scheduler.su
 
-.PHONY: clean-Src
+.PHONY: clean-JunkOS-2f-src
 
